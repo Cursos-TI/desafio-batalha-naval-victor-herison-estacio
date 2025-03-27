f3daff2eb7c7aadda3ca101bebafb58b4f3b1ba4
victor-herison-estacio <202503381755@alunos.estacio.br>
2025-03-27 21:46:27 +0000

    começo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,67 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[10][10] = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int navio_horizontal[3], navio_vertical[3] = {3,3,3};
+    int cordenada_horizontal[2] = {5, 2};
+    int  cordenada_vertical[2] = {3, 3};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    if(cordenada_horizontal[0] + 3 > 9 || cordenada_vertical[0] + 3 > 9 || cordenada_horizontal[1] + 3 > 9 || cordenada_vertical[1] + 3 > 9){
+        printf("Voce passou dos limites");
+        return;
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if(cordenada_horizontal[0] + j == cordenada_vertical[0])
+            {
+                printf("Colisão!! dois navios na mesma posição");
+                return;
+            }
+            if(cordenada_vertical[1] + j == cordenada_vertical[1])
+            {
+                printf("Colisão!! dois navios na mesma posição");
+                return;
+            } 
+        }
+    }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    for (int i = 0; i < 3; i++)
+    {
+        
+    }
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+char cabecalho = 'B';
+printf("   A");
+    for (int i = 0; i < 9; i++)
+    {
+        i < 8 ? printf(" %c", cabecalho) : printf(" %c\n", cabecalho);
+        cabecalho++;
+    }
+    
+    for (int i = 0; i < 10; i++)
+    {
+        i < 9  ? printf(" %d ", i + 1) :  printf("%d ", i + 1);
+        for (int j = 0; j < 10; j++)
+        {
+            j == 9 ? printf("%d\n", tabuleiro[i][j]) :  printf("%d ", tabuleiro[i][j]);
+        }
+        
+    }
+    
 
     return 0;
 }
